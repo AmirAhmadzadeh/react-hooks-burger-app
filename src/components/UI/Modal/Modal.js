3ad779992ef1e48d7bd3289ca11648b7af60c818
amir <you@example.com>
2019-11-02 14:26:11 +0330

    some Refactoring ...

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -31,42 +31,11 @@ function Modal(props) {
 
     );
 }
-
-export default Modal;
-
-// class Modal extends Component {
+// check this after 
+export default React.useMemo(Modal , (prevProps , nextProps) => (nextProps.show === prevProps.show) || (nextProps.show === prevProps.show) );
 
 
 
-//     shouldComponentUpdate(nextProps, nextStaet) {
-//         return (nextProps.show !== this.props.show) || (nextProps.children !== this.props.children);
-//     }
 
-
-//     render() {
-//         return (
-//             <Aux>
-//                 <Backdrop show={this.props.show}
-//                     clicked={this.props.closeModal}
-//                 />
-//                 <div className={classes.Modal}
-//                     style={
-//                         {
-//                             transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-//                             opacity: this.props.show ? '1' : '0'
-//                         }
-//                     }
-//                 >
-//                     {this.props.children}
-//                 </div>
-
-
-
-//             </Aux>
-
-//         );
-//     }
-
-// }
 
 
